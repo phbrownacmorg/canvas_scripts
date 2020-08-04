5d57b1ece7a634a88c4fc57f3ebbc8f3d206864f
Peter Brown <peter.brown@converse.edu>
2020-08-04 18:22:10 -0400

    Added csv_update.py

---
+++ csv_update.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3.7
+
+# Filter to correct the case of course fullnames for import to Canvas.
+# Input comes from one CSV file, and output is written to another.
+# Peter Brown <peter.brown@converse.edu>, 2020-08-04
+
+
+
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
+
