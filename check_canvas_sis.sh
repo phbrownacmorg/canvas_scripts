fa45e54a66ba71dd0c0392f3f6282f346a8bde6a
Peter Brown <peter.brown@converse.edu>
2022-07-14 15:30:59 -0400

    Changed check_canvas_sis.sh so it warns if not all input files are stale.

--- check_canvas_sis.sh
+++ check_canvas_sis.sh
@@ -27,33 +27,40 @@ MODTIME=`/bin/date  --reference=$STATFILE +%s`
 AGE=$((`/bin/date +%s` - $MODTIME))
 #echo age = $AGE
 
-# Check for stale input data
+# Check for stale input data.  This finds the *oldest* input file.
 INPUTDIR=/mnt/Canvas_Data
 # 15 hours.  No update from 8 PM - 7 AM shouldn't cause an alarm.
 STALE_INPUT_THRESHOLD=54000
-INPUT_AGE=0
+OLDEST_INPUT=0
+NEWEST_INPUT=86500000  # 1000 days
 for f in $INPUTDIR/*.csv ; do
     #echo $f
     INPUT_MOD=`/bin/date  --reference=$f +%s`
     #echo $INPUT_MOD
     THIS_AGE=$((`/bin/date +%s` - $INPUT_MOD))
     #echo $THIS_AGE
-    if [ $THIS_AGE -gt $INPUT_AGE ]; then
-	INPUT_AGE=$THIS_AGE
+    if [ $THIS_AGE -lt $NEWEST_INPUT ]; then
+	NEWEST_INPUT=$THIS_AGE
     fi
-    #echo $INPUT_AGE
+    if [ $THIS_AGE -gt $OLDEST_INPUT ]; then
+	OLDEST_INPUT=$THIS_AGE
+    fi
+    #echo $OLDEST_INPUT
 done
 
 
-if [ $INPUT_AGE -gt $STALE_INPUT_THRESHOLD ]; then
-    echo "CRITICAL - Stale input file(s) aged $INPUT_AGE sec"
+if [ $NEWEST_INPUT -gt $STALE_INPUT_THRESHOLD ]; then
+    echo "CRITICAL - Newest input file is aged $NEWEST_INPUT sec"
     exit $STATE_CRITICAL
+elif [ $OLDEST_INPUT -gt $STALE_INPUT_THRESHOLD ]; then
+    echo "WARN - Stale input file aged $OLDEST_INPUT sec"
+    exit $STATE_WARNING
 elif [ "$ALL_DIGITS" != "0" ]; then
     echo "OK - Last upload was $FILE_CONTENTS"
     exit $STATE_OK;
 elif [ $AGE -lt $CRIT ]; then
     echo "WARN - $FILE_CONTENTS - for $AGE sec"
-    exit $STATE_WARN
+    exit $STATE_WARNING
 else
     ${STATDIR}/autofix.py &
     echo "CRITICAL - $FILE_CONTENTS"
