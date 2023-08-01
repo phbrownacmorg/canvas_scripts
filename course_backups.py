77d42337d8cf028c603673a7c277dfd9070922e5
Peter Brown <phbrown@acm.org>
2023-08-01 02:11:41 -0400

    course_backups up through listing the courses

--- course_backups.py
+++ course_backups.py
@@ -9,28 +9,40 @@ import sys
 from filter_csv import read_from_csv
 from upload_csv import constants, get_access_token, bytesOrStrPrintable
 
+def print_course(course: dict) -> None:
+    print(course['canvas_course_id'], course['short_name'], course['status'])
+
 def print_courses(courses: list[dict]) -> None:
     for course in courses:
-        print(course['canvas_course_id'], course['course_id'], course['status'])
+        print_course(course)
 
-def read_course_list(term: str) -> list[int]:
+def read_course_list(term: str) -> list[dict]:
     reportsdir = Path.home().joinpath('Documents', 'DEd', 'Canvas', 'course_backups', 'reports')
-    print(reportsdir)
+    # print(reportsdir)
     courses: list[dict] = read_from_csv(reportsdir.joinpath(term + '-courses.csv'))
-    courses = list(filter(lambda c: c['status'] == 'active', courses))
+    courses = [c for c in courses if c['status'] == 'active'] # Filter out any courses that were not published
 
+    # Filter out courses that were cross-listed, even if they were published
     xlist: list[dict] = read_from_csv(reportsdir.joinpath(term + '-xlist.csv'))
+    xlist_nums = [item['canvas_nonxlist_course_id'] for item in xlist]
+    #print(xlist_nums)
+    courses = [c for c in courses if c['canvas_course_id'] not in xlist_nums]
+    courses.sort(key=lambda c : c['short_name'])
+    #result = [c['canvas_course_id'] for c in courses]
 
-    print_courses(courses)
-    result: list[int] = []
+    return courses
 
-
-    return result
-
+def back_up_course(term: str, course: dict) -> None:
+    pass
 
 def main(argv: list[str]) -> int:
     term: str = '2223-SP'
-    courselist: list[int] = read_course_list(term)
+    courselist: list[dict] = read_course_list(term)
+    #print_courses(courselist)
+
+    for c in courselist:
+        print_course(c)
+        back_up_course(term, c)
     return 0
 
 if __name__ == '__main__':
