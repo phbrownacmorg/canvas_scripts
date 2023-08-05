b9f612a91c65d6b38c978effc3e4b4226d64f5d0
Peter Brown <phbrown@acm.org>
2023-08-04 21:57:06 -0400

    Fixed course shortnames (actually course_id)

--- course_backups.py
+++ course_backups.py
@@ -13,27 +13,70 @@ from filter_csv import read_from_csv
 from upload_csv import constants, get_access_token, bytesOrStrPrintable
 
 def print_course(course: dict, endstr: str = '\n') -> None:
-    print(course['canvas_course_id'], course['short_name'], course['status'], end=endstr, flush=True)
+    print(course['canvas_course_id'], course['course_id'], course['status'], end=endstr, flush=True)
 
 def print_courses(courses: list[dict]) -> None:
     for course in courses:
         print_course(course)
 
+def make_course_id(id_list: list[str]) -> str:
+    assert len(id_list) > 1 # Must be at least two ID's
+    # Extract the term (with its leading hyphen) from the first course
+    term = id_list[0][-8:]
+    # Remove all the terms
+    id_list = list(map(lambda elt: elt[:-8], id_list))
+    id_list.sort() # Ensure that common prefixes happen together
+
+    result = id_list[0]
+    prefix = result[:3]
+    for i in range(1, len(id_list)):
+        if id_list[i][:3] == prefix:
+            result = result + '/' + id_list[i][3:]
+        else:
+            result = result + '/' + id_list[i]
+            prefix = id_list[i][:3]
+    return result + term
+
+def fix_course_ids(courselist: list[dict], xlist: list[dict]) -> list[dict]:
+    coursedict: dict[str, dict] = {}
+    # Put the courses in a dictionary for quick reference
+    for c in courselist:  
+        coursedict[c['course_id']] = c
+
+    # Iterate over the xlists, adjusting the courses as needed
+    i = 0
+    xlist = sorted(xlist, key=lambda elt: elt['nonxlist_course_id'])
+    xlist = sorted(xlist, key=lambda elt: elt['xlist_course_id'])
+    ids: list[str] = []
+    while i < len(xlist):
+        host_id = xlist[i]['xlist_course_id']
+        if len(ids) == 0:
+            ids.append(host_id)
+        guest_id = xlist[i]['nonxlist_course_id']
+        ids.append(guest_id)
+        try:
+            coursedict[guest_id]['status'] = host_id # guest courses are never active
+        except KeyError as e:
+            print('KeyError:', e, 'not found: wrong term?')
+            ids.remove(guest_id)
+
+        if (i == len(xlist) - 1) or (xlist[i+1]['xlist_course_id'] != host_id):
+            if len(ids) > 1:
+                coursedict[host_id]['course_id'] = make_course_id(ids)
+            ids = []
+        i += 1
+    return sorted(coursedict.values(), key=lambda elt: elt['course_id'])
+
+
 def read_course_list(term: str) -> list[dict]:
     reportsdir = Path.home().joinpath('Documents', 'DEd', 'Canvas', 'course_backups', 'reports')
     # print(reportsdir)
     courses: list[dict] = read_from_csv(reportsdir.joinpath(term + '-courses.csv'))
-    courses = [c for c in courses if c['status'] == 'active'] # Filter out any courses that were not published
-
-    # Filter out courses that were cross-listed, even if they were published
     xlist: list[dict] = read_from_csv(reportsdir.joinpath(term + '-xlist.csv'))
-    xlist_nums = [item['canvas_nonxlist_course_id'] for item in xlist]
-    #print(xlist_nums)
-    courses = [c for c in courses if c['canvas_course_id'] not in xlist_nums]
-    courses.sort(key=lambda c : c['short_name'])
-    #result = [c['canvas_course_id'] for c in courses]
-
+    courses = fix_course_ids(courses, xlist)
+    courses = [c for c in courses if c['status'] == 'active'] # Filter out any courses that were not published
     return courses
+
 
 def start_course_backup(course_ID: int) -> dict:
     """Initiate a course backup for COURSE.  Return the result of the course backup, as a dictionary."""
@@ -131,9 +174,10 @@ def maybe_download_backup(term: str, course: dict) -> None:
        it in the proper directory with the right filename."""
     backup_data = wait_for_completion(course['canvas_course_id'])
     output_dir = Path.home().joinpath('Documents', 'DEd', 'Canvas', 'course_backups')
-    filename = term + '_' + course['short_name'].replace('/', '+') + '_' + backup_data['filename']
+    filename = term + '_' + course['course_id'].replace('/', '+') + '_' + backup_data['filename']
+    filepath = output_dir.joinpath(filename)
 
-    if output_dir.joinpath(filename).exists():
+    if filepath.exists() and filepath.stat().st_size == backup_data['size']:
         print('downloaded already')
     else:
         urlparts = urllib.parse.urlparse(backup_data['url'])
@@ -154,16 +198,20 @@ def maybe_download_backup(term: str, course: dict) -> None:
 #     maybe_download_backup(term, course, backup_data)
 
 def main(argv: list[str]) -> int:
-    term: str = '2223-SP'
+    term: str = '2223-JA'
     courselist: list[dict] = read_course_list(term)
     print(len(courselist), 'courses')
-    #print_courses(courselist)
+    # print_courses(courselist)
+
     print(time.asctime(), flush=True)
-    # courselist = courselist[:10]
+    #courselist = courselist[350:360]
+    i = 0
     for c in courselist:
+        print(i, end=' ')
         print_course(c, ': ')
         maybe_create_backup(c['canvas_course_id'])
         maybe_download_backup(term, c)
+        i += 1
     # for c in courselist:
     #     print_course(c, ': ')
 
