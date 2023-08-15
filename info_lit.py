e326ac4fc6bf94839a420cbeb1e664c1df96e4a1
Peter Brown <phbrown@acm.org>
2023-08-15 02:33:26 -0400

    Started info_lit.py

---
+++ info_lit.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+from pathlib import Path
+import sys
+import urllib.parse
+from filter_csv import read_from_csv
+from upload_csv import constants, get_access_token, bytesOrStrPrintable
+
+def main(argv: list[str]) -> int:
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
